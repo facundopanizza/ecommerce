9da4ce43ae93c12a4eac6695348760612e1bef0d
Facundo Panizza <facundo@panizza.dev>
2020-12-08 13:01:16 -0300

    Added product type and prodct type endpoint and include relationships on getProducts

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -28,5 +28,17 @@ namespace API.Controllers
         {
             return await _productRepository.GetProductByIdAsync(id);
         }
+
+        [HttpGet("brands")]
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
+        {
+            return  Ok(await _productRepository.GetProductBrandAsync());
+        }
+
+        [HttpGet("types")]
+        public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes()
+        {
+            return  Ok(await _productRepository.GetProductTypeAsync());
+        }
     }
 }
